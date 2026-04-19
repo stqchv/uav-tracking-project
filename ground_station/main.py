1c660590bbcbce31eb1e6c6e18a793605ad0bbc9
stqchv <tomek_stas@onet.pl>
2026-04-19 17:23:08 +0200

    Created main schema.

---
+++ ground_station/main.py
@@ -0,0 +1,10 @@
+from communication import command_sender, video_receiver
+from control import convert
+from vision import person_detector
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
