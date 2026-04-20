db2bc2878b5083526097fdd7988f458c0b3c7176
stqchv <tomek_stas@onet.pl>
2026-04-20 22:31:35 +0200

    Created own thread for video stream reciever.

--- ground_station/communication/video_receiver.py
+++ ground_station/communication/video_receiver.py
@@ -1,26 +1,55 @@
 import cv2
+import threading
+
+class VideoStream:
+    def __init__(self, port=5000):
+        pipeline = (
+            f"udpsrc port={port} ! "
+            "h264parse ! "
+            "avdec_h264 ! "
+            "videoconvert ! "
+            "video/x-raw,format=BGR ! "
+            "appsink sync=false drop=true max-buffers=1"
+        )
+
+        self.cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+        self.ret = False
+        self.frame = None
+        self.running = False
+
+        if self.cap.isOpened():
+            self.running = True
+
+    def start(self):
+        self.thread = threading.Thread(target=self.update, args=(), daemon=True)
+        self.thread.start()
+        return self
+    
+    def update(self):
+        while self.running:
+            if self.cap.isOpened():
+                self.ret, self.frame = self.cap.read()
+
+    def read(self):
+        return self.ret, self.frame
+    
+    def stop(self):
+        self.running = False
+        if self.thread.is_alive():
+            self.thread.join()
+        self.cap.release()
+    
 
 def start_video_stream(port=5000):
     print(f"Waiting for the stream on port {port}...")
-
-    pipeline = (
-        f"udpsrc port={port} ! "
-        "h264parse ! "
-        "avdec_h264 ! "
-        "videoconvert ! "
-        "video/x-raw,format=BGR ! "
-        "appsink sync=false drop=true max-buffers=1"
-    )
-
-    cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
-
-    if not cap.isOpened():
-        print("Unable to recieve stream.")
+    stream = VideoStream(port)
+    
+    if not stream.running:
+        print("Error: Could not open stream.")
         return None
     
-    print("Connected to video stream.")
-    return cap
+    print("Connected to video stream. Starting thread...")
+    return stream.start()
 
-def recieve_frame(cap):
-    ret, frame = cap.read()
-    return ret, frame
+def recieve_frame(stream):
+    return stream.read()
--- ground_station/main.py
+++ ground_station/main.py
@@ -1,31 +1,32 @@
 import cv2
+import time
 
 from communication.video_receiver import start_video_stream, recieve_frame
 from control import convert
 from vision import person_detector
 
 def main():
-    cap = start_video_stream(port=5000)
-
-    if cap is None:
-        return
+    video_stream = start_video_stream(port=5000)
+    if video_stream is None: return
+    time.sleep(0.5)
     
     try:
         while True:
-            ret, frame = recieve_frame(cap)
+            ret, frame = recieve_frame(video_stream)
 
-            if not ret:
-                print("Frame lost or connection failed.")
-                break
+            if not ret or frame is None:
+                print("Lost frame. Waiting for another...")
+                continue
 
             # YOLO detection
+            time.sleep(0.4)
 
             cv2.imshow("Base Station - Camera Preview", frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
     finally:
-        cap.release()
+        video_stream.stop()
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
