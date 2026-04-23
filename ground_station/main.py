4028c0348eb6bbf5c829896a61f8b46c94161e91
stqchv <tomek_stas@onet.pl>
2026-04-23 11:25:49 +0200

    Created vision module with yolov8 model and person_detector class.

--- ground_station/main.py
+++ ground_station/main.py
@@ -3,27 +3,34 @@ import time
 
 from communication.video_receiver import start_video_stream, receive_frame
 from control import convert
-from vision import person_detector
+from vision.person_detector import PersonDetector
 
 def main():
     video_stream = start_video_stream(port=5000)
     if video_stream is None: return
     time.sleep(0.5)
+
+    vision = PersonDetector()
     
     try:
         while True:
             ret, frame = receive_frame(video_stream)
 
             if not ret or frame is None:
-                print("Lost frame. Waiting for another...")
+                # print("Lost frame. Waiting for another...")
                 continue
 
             # YOLO detection
-            # time.sleep(0.4)
+            error_x, error_y, annotated_frame = vision.get_target_error(frame)
 
-            cv2.imshow("Base Station - Camera Preview", frame)
+            # CONTROLL
+            # if error_x is not None:
+            #     print(f"Błąd X: {error_x}, Y: {error_y}")
+
+            cv2.imshow("Base Station - Tracking Live", annotated_frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("Closing ground station...")
                 break
     finally:
         video_stream.stop()
---
+++ ground_station/vision/person_detector.py
@@ -0,0 +1,54 @@
+import cv2
+import time
+from ultralytics import YOLO
+
+class PersonDetector:
+    def __init__(self, model_path='vision/yolov8n.pt'):
+        print(f"[VISION] Reading model from: {model_path}")
+        self.model = YOLO(model_path)
+
+        self.width = None
+        self.height = None
+        self.center_x = None
+        self.center_y = None
+
+    def get_target_error(self, frame):
+        start_time = time.time()
+
+        if self.width is None:
+            self.height, self.width = frame.shape[:2]
+            self.center_x = self.width // 2
+            self.center_y = self.height // 2
+            print(f"[VISION] Set image middle point (UDP): ({self.center_x}, {self.center_y})")
+        
+        annotated_frame = frame.copy()
+
+        results = self.model.predict(frame, classes=0, conf=.5, verbose=False)
+
+        cv2.drawMarker(annotated_frame, (self.center_x, self.center_y), (0, 0, 255), cv2.MARKER_CROSS, 20, 2)
+        
+        error_x, error_y = None, None
+
+        for r in results:
+            if len(r.boxes) > 0:
+                box = r.boxes[0]
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                
+                person_cx = (x1 + x2) // 2
+                person_cy = (y1 + y2) // 2
+                
+                error_x = person_cx - self.center_x
+                error_y = person_cy - self.center_y
+                
+                cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.circle(annotated_frame, (person_cx, person_cy), 5, (0, 255, 0), -1)
+                cv2.line(annotated_frame, (self.center_x, self.center_y), (person_cx, person_cy), (0, 255, 255), 2)
+                
+                cv2.putText(annotated_frame, f"Blad X: {error_x}px | Blad Y: {error_y}px", (20, 90), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+
+        # FPS 
+        fps = 1 / (time.time() - start_time)
+        cv2.putText(annotated_frame, f"FPS: {fps:.1f}", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+        return error_x, error_y, annotated_frame
